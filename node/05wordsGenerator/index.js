96a46a9b1aaaf13a0fbc93edb0e619a3f6ded149
wAiwAi <822458398@qq.com>
2022-09-04 00:07:54 +0800

    9.4

--- node/05wordsGenerator/index.js
+++ node/05wordsGenerator/index.js
@@ -6,18 +6,54 @@ import { generate } from "./lib/generate.js";
 import { createRandomPicker } from "./lib/random.js";
 import { mkdir } from "fs/promises";
 
+import commandLineArgs from "command-line-args";
+import commandLineUsage from "command-line-usage";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-function loadCorpus(src) {
-  const path = resolve(__dirname, src);
-  const data = readFileSync(path, { encoding: "utf-8" });
-  return JSON.parse(data);
-}
 const corpus = loadCorpus("corpus/data.json");
 const pickTitle = createRandomPicker(corpus.title);
-const title = pickTitle();
-const article = generate(title, { corpus });
-saveCorpus(title, article);
+const optionDefinitions = [
+  { name: "help" },
+  { name: "title", alias: "t", type: String },
+  { name: "min", type: Number },
+  { name: "max", type: Number },
+];
+
+const sections = [
+  { header: "狗屁不通文章生成器", content: "生成随机的文章" },
+  {
+    header: "Options",
+    optionList: [
+      {
+        name: "title",
+        typeLabel: "{underline string}",
+        description: "文章的主题",
+      },
+      {
+        name: "min",
+        typeLabel: "{underline number}",
+        description: "文章的最小字数",
+      },
+      {
+        name: "max",
+        typeLabel: "{underline number}",
+        description: "文章的最大字数",
+      },
+    ],
+  },
+];
+const usage = commandLineUsage(sections); //生成帮助文档
+const options = commandLineArgs(optionDefinitions);
+if ("help" in options) {
+  console.log(usage);
+  process.exit();
+} else {
+  const title = options.title || pickTitle();
+  const article = generate(title, { corpus, ...options });
+  const output = saveCorpus(title, article);
+  console.log(`生成成功！文章保存于 :${output}`);
+}
+
 function saveCorpus(title, article) {
   const outputDir = resolve(__dirname, "output");
   const time = new Date().valueOf();
@@ -29,4 +65,24 @@ function saveCorpus(title, article) {
   const text = `${title}\n\n ${article.join("\n    ")}`;
   writeFileSync(outputFile, text);
   return outputFile;
+}
+function parseOption(options = {}) {
+  const argv = process.argv;
+  for (let i = 0; i < argv.length; i++) {
+    const cmd = argv[i - 1];
+    const val = argv[i];
+    if (cmd === "--title") {
+      options.title = val;
+    } else if (cmd === "--min") {
+      options.min = Number(val);
+    } else if (cmd === "--max") {
+      options.max = Number(val);
+    }
+  }
+  return options;
+}
+function loadCorpus(src) {
+  const path = resolve(__dirname, src);
+  const data = readFileSync(path, { encoding: "utf-8" });
+  return JSON.parse(data);
 }
